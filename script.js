89a1fa3c537ce86beac256b59fd698d74e751a6e
Abdelilah Eddalili <166046001+Abdelilah2009@users.noreply.github.com>
2024-11-16 16:14:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 
 
-const navbar=document.getElementById('navbar')
 
-window.addEventListener('scroll',()=>{
-    if (window.scrollY>100) {
-        navbar.classList.add('active')
-    }else{
-        navbar.classList.remove('active')
+// const navbar=document.getElementById('navbar')
+
+// window.addEventListener('scroll',()=>{
+//     if (window.scrollY>100) {
+//         navbar.classList.add('active')
+//     }else{
+//         navbar.classList.remove('active')
         
-    }
+//     }
 
-})
+// })
